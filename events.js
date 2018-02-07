648057e92bc7549d2d83e5289dd1c12cbdb2722b
Sarah McLaughlin <sarahmclaughlin0@gmail.com>
2018-02-08 12:11:01 +1300

    js events

--- events.js
+++ events.js
@@ -9,6 +9,7 @@ function start () {
   one()
   two()
   three()
+  four()
     
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -36,7 +37,11 @@ function three () {
   three.addEventListener('mouseleave', makeWhite)
 }
 
-// CREATE FUNCTION four HERE
+function four () {
+  var four = document.getElementById('four')
+  four.addEventListener('click', makeRed)
+  four.addEventListener('dblclick', makeWhite)
+}
 
 // Changes the background color of event's target
 function makeBlue (evt) {
@@ -49,6 +54,10 @@ function makeGreen (evt) {
 
 function makeOrange (evt) {
   evt.target.style.backgroundColor = 'orange'
+}
+
+function makeRed (evt) {
+  evt.target.style.backgroundColor = 'red'
 }
 
 function makeWhite (evt) {
